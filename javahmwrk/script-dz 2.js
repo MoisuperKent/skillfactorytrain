7dad47fb2b92f838902275f4abf116e4409652f4
MoisuperKent <80202505+MoisuperKent@users.noreply.github.com>
2026-01-30 13:42:44 +0300

    JavaScriptHomework

---
+++ javahmwrk/script-dz 2.js
@@ -0,0 +1,9 @@
+const discont=prompt("Введите промокод");
+if (discont!==null) {
+    if (discont.toLowerCase()=="скидка") {
+        alert("Промокод успешно применён");
+    }
+    else {
+        alert("Промокод не работает");
+    }
+}
---
+++ javahmwrk/script-dz 3.js
@@ -0,0 +1,18 @@
+const userName=prompt("Введите имя");
+if (userName!==null && userName.trim()!=="") {
+    if (isFinite(userName)) {
+        alert("Имя не может быть в таком формате");
+    }
+    else {
+        let age=prompt("Введите год рождения")
+        if (isFinite(age) && age.trim()!=="") {
+            alert(`Добро пожаловать, ${userName} ${2026-age}!`)
+        }
+        else {
+            alert("Год рождения должен быть числом!")
+        }
+    }
+}
+else {
+    alert("Вы не ввели имя!")
+}
