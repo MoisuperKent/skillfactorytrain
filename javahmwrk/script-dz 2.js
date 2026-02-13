7046cb165f9ba9866aed4b75e0650e6612d6c388
MoisuperKent <80202505+MoisuperKent@users.noreply.github.com>
2026-02-13 18:46:28 +0300

    Delete javahmwrk directory

--- javahmwrk/script-dz 2.js
+++
@@ -1,9 +0,0 @@
-const discont=prompt("Введите промокод");
-if (discont!==null) {
-    if (discont.toLowerCase()=="скидка") {
-        alert("Промокод успешно применён");
-    }
-    else {
-        alert("Промокод не работает");
-    }
-}
